7f8fb3f6a21b84d4eeafcb2779ccefa191299bd0
Darryl Brooks <darryl.brooks@myfuelmaster.com>
2021-06-28 23:40:39 -0500

    Fixed rotate bug

--- components/Stonks.tsx
+++ components/Stonks.tsx
@@ -1,58 +1,56 @@
-import { Box, Center, Flex } from "@chakra-ui/react";
-import styled from "@emotion/styled";
-import { transform } from "framer-motion";
-import { useState } from "react";
-import { CSSProperties } from "react";
+import { Box, Center, Flex } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+import { transform } from 'framer-motion';
+import { useState } from 'react';
+import { CSSProperties } from 'react';
 
 interface Props {
-  stonk: any;
-  idx: string;
+	stonk: any;
+	idx: string;
 }
 const scene: CSSProperties = {
-  perspective: "600px",
+	perspective: '600px',
 };
 const card: CSSProperties = {
-  position: "relative",
-  transition: "transform 0.8s",
-  transformStyle: "preserve-3d",
+	position: 'relative',
+	transition: 'transform 0.8s',
+	transformStyle: 'preserve-3d',
 };
 const card_face: CSSProperties = {
-  height: "100%",
-  width: "100%",
-  backfaceVisibility: "hidden",
+	height: '100%',
+	width: '100%',
+	backfaceVisibility: 'hidden',
 };
 const Card = styled.div`
-  color: blue;
-  border-radius: 15px;
-  border: 1px solid black;
-  padding: 1em;
-  box-shadow: 5px 5px black;
+	color: blue;
+	border-radius: 15px;
+	border: 1px solid black;
+	padding: 1em;
+	box-shadow: 5px 5px black;
+	transform: ${(props) => (props.clicked ? 'rotateY(180deg)' : 'rotateY(0deg)')};
 `;
 export default function Stonks({ stonk, idx }: Props) {
-  const [clicked, setClicked] = useState<Boolean>(false);
-  return (
-    <Box style={scene}>
-      <Card
-        key={idx}
-        style={card}
-        onClick={() => {
-          const doc = document.getElementById("card");
-          setClicked(!clicked);
-          doc.style.transform = clicked ? "rotateY(180deg)" : "rotateY(0deg)";
-        }}
-        id='card'
-      >
-        <Center style={card_face}>
-          <Flex direction='column'>
-            <Box>{stonk.symbol}</Box>
-            <Box>{stonk.name}</Box>
-            <Box>${stonk.priceUsd} USD</Box>
-          </Flex>
-        </Center>
-        <Box style={card_face} transform='rotateY(180deg)'>
-          Whut
-        </Box>
-      </Card>
-    </Box>
-  );
+	const [clicked, setClicked] = useState<Boolean>(false);
+	return (
+		<Box style={scene} key={idx}>
+			<Card
+				key={idx}
+				style={card}
+				clicked={clicked}
+				onClick={() => {
+					setClicked(!clicked);
+				}}
+			>
+				<Center style={card_face}>
+					<Flex direction='column'>
+						<Box>{stonk.symbol}</Box>
+						<Box>{stonk.name}</Box>
+					</Flex>
+				</Center>
+				<Box style={card_face} transform='rotateY(180deg)'>
+					${stonk.priceUsd} USD
+				</Box>
+			</Card>
+		</Box>
+	);
 }
