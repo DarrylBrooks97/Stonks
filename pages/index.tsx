f5053fad333e1a8c85f9e13e9ad31b07750e456c
DarrylBrooks97 <darrylbrooks13@gmail.com>
2021-06-28 22:19:23 -0500

    Add card rotation effect. Still has bugs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,44 @@
-import { Box, Center, Input, SimpleGrid } from '@chakra-ui/react';
-import { useState } from 'react';
-import Stonks from '../components/Stonks';
+import { Box, Center, Input, SimpleGrid } from "@chakra-ui/react";
+import { useState } from "react";
+import Stonks from "../components/Stonks";
 
 interface Props {
-	stonks: any;
+  stonks: any;
 }
 
 export default function Home({ stonks }: Props) {
-	const [searchedStonks, setSearchedStonks] = useState<any>(stonks);
+  const [searchedStonks, setSearchedStonks] = useState<any>(stonks);
 
-	const searchStonks = (query: string) => {
-		setSearchedStonks(stonks.filter((el) => el.id.includes(query)));
-	};
+  const searchStonks = (query: string) => {
+    setSearchedStonks(stonks.filter((el) => el.id.includes(query)));
+  };
 
-	return (
-		<Box>
-			<Center>
-				<Input
-					placeholder='Search for stonk symbol...'
-					w='xl'
-					onChange={(e) => searchStonks(e.target.value)}
-				/>
-			</Center>
-			<SimpleGrid columns={4} spacing={10}>
-				{searchedStonks?.map((stonk, idx) => {
-					return <Stonks stonk={stonk} idx={idx} />;
-				})}
-			</SimpleGrid>
-		</Box>
-	);
+  return (
+    <Box>
+      <Center>
+        <Input
+          placeholder='Search for stonk symbol...'
+          w='xl'
+          onChange={(e) => searchStonks(e.target.value)}
+        />
+      </Center>
+      <SimpleGrid columns={3} spacing={10} padding={20}>
+        {searchedStonks?.map((stonk, idx) => {
+          return <Stonks stonk={stonk} idx={idx} key={idx} />;
+        })}
+      </SimpleGrid>
+    </Box>
+  );
 }
 
 export async function getStaticProps() {
-	const { data }: any = await (
-		await fetch('http://api.coincap.io/v2/assets')
-	).json();
+  const { data }: any = await (
+    await fetch("http://api.coincap.io/v2/assets")
+  ).json();
 
-	return {
-		props: {
-			stonks: data,
-		},
-	};
+  return {
+    props: {
+      stonks: data,
+    },
+  };
 }
